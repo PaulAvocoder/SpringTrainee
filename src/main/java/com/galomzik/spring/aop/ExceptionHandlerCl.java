189bf03fa8e2cd24c31e6e7013145722221ec3ce
Paul <Paul@mail.ru>
2024-02-01 12:01:30 +0300

    upd ExcHand clean

--- src/main/java/com/galomzik/spring/aop/ExceptionHandlerCl.java
+++ src/main/java/com/galomzik/spring/aop/ExceptionHandlerCl.java
@@ -1,31 +1,29 @@
 package com.galomzik.spring.aop;
 
-import com.galomzik.spring.exception.BadDataException;
-import com.galomzik.spring.exception.ConflictException;
-import com.galomzik.spring.exception.NotFoundException;
-import com.galomzik.spring.exception.UnauthorizedException;
+import com.galomzik.spring.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler(BadDataException.class)
+public class ExceptionHandlerCl {
+    @ExceptionHandler(BadDataException.class)
     public ResponseEntity<String> handlerBadData(BadDataException badDataException) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(badDataException.getMessage());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(ConflictException.class)
+    @ExceptionHandler(ConflictException.class)
     public ResponseEntity<String> conflictHandler(ConflictException conflictException) {
         return ResponseEntity.status(409).body(conflictException.getMessage());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler
     public ResponseEntity<String> notFoundHandler(NotFoundException notFoundException) {
         return ResponseEntity.status(404).body(notFoundException.getMessage());
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler
     public ResponseEntity<String> unauthorizedHandler(UnauthorizedException unauthorizedException) {
         return ResponseEntity.status(401).body(unauthorizedException.getMessage());
     }
